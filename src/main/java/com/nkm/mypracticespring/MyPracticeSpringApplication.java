7140a45d797f0db4dc458df95b46d6da49806d84
manhnk <manh.nk@cmctelecom.vn>
2024-08-28 10:59:53 +0000

    init project

---
+++ src/main/java/com/nkm/mypracticespring/MyPracticeSpringApplication.java
@@ -0,0 +1,13 @@
+package com.nkm.mypracticespring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyPracticeSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyPracticeSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nkm/mypracticespring/MyPracticeSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nkm.mypracticespring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyPracticeSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
